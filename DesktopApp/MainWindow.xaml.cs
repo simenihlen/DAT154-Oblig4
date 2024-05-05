29db066aa61b68d2a15fb1bc5df99cec45309a14
Simen Ihlen <simenihlen@gmail.com>
2024-05-06 00:46:46 +0200

    Start på DesktopApp med All Rooms liste og søk funksjon

--- DesktopApp/MainWindow.xaml.cs
+++ DesktopApp/MainWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Text;
+﻿using HotelLibrary.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -14,8 +17,47 @@ namespace DesktopApp {
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window {
+
+        private readonly HotelDbContext dx = new();
+        private readonly LocalView<Roomdatum> room;
+        private readonly LocalView<Bookingdatum> booking;
+        private readonly LocalView<User> user;
+
         public MainWindow() {
             InitializeComponent();
+
+            room = dx.Roomdata.Local;
+            booking = dx.Bookingdata.Local;
+            user = dx.Users.Local;
+
+            dx.Roomdata.Load();
+            dx.Bookingdata.Load();
+            dx.Users.Load();
+
+            allRooms();
+        }
+
+        private void SearchButton_Click(object sender, RoutedEventArgs e) {
+            dx.Roomdata.Load();
+            string searchText = searchBox.Text.Trim().ToLower();
+            
+            if(string.IsNullOrEmpty(searchText) ) {
+                allRooms();
+            } else if (int.TryParse(searchText, out int RoomNumber)) {
+                var filteredList = dx.Roomdata.Where(r => r.RoomNumber == RoomNumber).ToList();
+                roomList.ItemsSource = filteredList;
+            }
+        }
+
+        private void allRooms() {
+            dx.Roomdata.Load();
+            var filteredList = dx.Roomdata.Select (r => new {
+                RoomNumber = r.RoomNumber,
+                NumberOfBeds = r.NumberOfBeds,
+                RoomSize = r.RoomSize,
+                RoomQuality = r.RoomQuality
+            }).ToList();
+            roomList.ItemsSource = filteredList;
         }
     }
 }
