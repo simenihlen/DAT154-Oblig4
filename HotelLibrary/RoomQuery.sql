723ee48da9b29340d9f433047610b1861e85003f
Simen Ihlen <simenihlen@gmail.com>
2024-05-05 22:54:17 +0200

    Endring på RoomQuery

--- HotelLibrary/RoomQuery.sql
+++ HotelLibrary/RoomQuery.sql
@@ -1,17 +1,17 @@
-INSERT INTO Rooms (room_number, number_of_beds, room_size, room_quality)
-VALUES ('101', 2, 'Medium', 'Standard'),
-       ('102', 1, 'Small', 'Economy'),
-       ('103', 3, 'Large', 'Suit'),
-       ('104', 1, 'Small', 'Economy'),
-       ('201', 2, 'Medium', 'Standard'),
-       ('202', 1, 'Small', 'Economy'),
-       ('203', 3, 'Large', 'Suit'),
-       ('204', 1, 'Small', 'Economy'),
-       ('301', 2, 'Medium', 'Standard'),
-       ('302', 1, 'Small', 'Economy'),
-       ('303', 3, 'Large', 'Suit'),
-       ('304', 1, 'Small', 'Economy'),
-       ('401', 2, 'Medium', 'Standard'),
-       ('402', 1, 'Small', 'Economy'),
-       ('403', 3, 'Large', 'Suit'),
-       ('404', 1, 'Small', 'Economy');
+INSERT INTO roomdata (room_number, number_of_beds, room_size, room_quality)
+VALUES (101, 2, 2, 'Standard'), --Medium
+       (102, 1, 1, 'Economy'), --Small
+       (103, 3, 3, 'Suit'), --Large
+       (104, 1, 1, 'Economy'),
+       (201, 2, 2, 'Standard'),
+       (202, 1, 1, 'Economy'),
+       (203, 3, 3, 'Suit'),
+       (204, 1, 1, 'Economy'),
+       (301, 2, 2, 'Standard'),
+       (302, 1, 1, 'Economy'),
+       (303, 3, 3, 'Suit'),
+       (304, 1, 1, 'Economy'),
+       (401, 2, 2, 'Standard'),
+       (402, 1, 1, 'Economy'),
+       (403, 3, 3, 'Suit'),
+       (404, 1, 1, 'Economy');
